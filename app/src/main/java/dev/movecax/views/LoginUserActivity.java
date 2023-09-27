54398769058c79ae16a0b0d27cb16028bed118b6
Alejandro-1003 <sifuentesabanto03fgmail.com>
2023-09-26 23:19:58 -0500

    interfaz login add

---
+++ app/src/main/java/dev/movecax/views/LoginUserActivity.java
@@ -0,0 +1,16 @@
+package dev.movecax.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dev.movecax.R;
+
+public class LoginUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_user);
+    }
+}
