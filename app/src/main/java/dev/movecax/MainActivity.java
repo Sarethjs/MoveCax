1733094fde86f568f304d72a0841f58d9b746f0b
DanielCastillo2212 <danielandre.castillo@gmail.com>
2023-10-03 16:05:44 -0500

    Navigation and perfil

--- app/src/main/java/dev/movecax/MainActivity.java
+++ app/src/main/java/dev/movecax/MainActivity.java
@@ -1,15 +1,63 @@
 package dev.movecax;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import dev.movecax.Fragment.FirstFragment;
+import dev.movecax.Fragment.FourthFragment;
+import dev.movecax.Fragment.SecondFragment;
+import dev.movecax.Fragment.ThirdFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirstFragment firstFragment = new FirstFragment();
+    SecondFragment secondFragment = new SecondFragment();
+    ThirdFragment thirdFragment = new ThirdFragment();
+    FourthFragment fourthFragment = new FourthFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        loadFragment(firstFragment);
+    }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            int itemId = item.getItemId();
+            if (itemId == R.id.firstFragment) {
+                loadFragment(firstFragment);
+                return true;
+            } else if (itemId == R.id.secondFragment) {
+                loadFragment(secondFragment);
+                return true;
+            } else if (itemId == R.id.thirdFragment) {
+                loadFragment(thirdFragment);
+                return true;
+            }
+            else if (itemId == R.id.fourthFragment) {
+                loadFragment(fourthFragment);
+                return true;
+            }
+            return false;
+        }
+    };
+
+    public void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.commit();
     }
 }
